4d48b715c03e3fd0dd958cd1524159812d031ba8
huangr0867 <97621381+huangr0867@users.noreply.github.com>
2022-02-02 19:05:29 -0800

    HW

--- Unit06/src/PerfectRunner.java
+++ Unit06/src/PerfectRunner.java
@@ -9,6 +9,38 @@ public class PerfectRunner
 	public static void main( String args[] )
 	{
 		//add test cases
+		
+		Perfect a = new Perfect();
+		
+		a.setNum(496);
+		out.println(a);
+		
+		a.setNum(45);
+		out.println(a);
+		
+		a.setNum(6);
+		out.println(a);
+		
+		a.setNum(14);
+		out.println(a);
+		
+		a.setNum(8128);
+		out.println(a);
+		
+		a.setNum(1245);
+		out.println(a);
+		
+		a.setNum(33);
+		out.println(a);
+		
+		a.setNum(28);
+		out.println(a);
+		
+		a.setNum(27);
+		out.println(a);
+		
+		a.setNum(33550336);
+		out.println(a);
 																
 	}
 }
